263b877d7cc5a6381f5c51b631b62095a458fa70
Vladislav <kentuukq@gmail.com>
2024-04-23 12:58:46 +0300

    Заполнение информации о профиле

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -91,8 +91,48 @@ private fun AppDrawerHeader() {
 }
 
 @Composable
-fun ProfileInfo() {
-  //TODO add your code here
+fun ProfileInfo(modifier: Modifier = Modifier) {
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxWidth()
+      .padding(top = 16.dp)
+  ) {
+    val (karmaItem, divider, ageItem) = createRefs()
+    val colors = MaterialTheme.colors
+
+    ProfileInfoItem(
+      Icons.Filled.Star,
+      R.string.default_karma_amount,
+      R.string.karma,
+      modifier = modifier.constrainAs(karmaItem) {
+        centerVerticallyTo(parent)
+        start.linkTo(parent.start)
+      }
+    )
+    Divider(
+      modifier = modifier
+        .width(1.dp)
+        .constrainAs(divider) {
+          centerVerticallyTo(karmaItem)
+          centerHorizontallyTo(parent)
+          height = Dimension.fillToConstraints
+        },
+      color = colors.onSurface.copy(alpha = .2f)
+    )
+    ProfileInfoItem(Icons.Filled.ShoppingCart,
+      R.string.default_reddit_age_amount,
+      R.string.reddit_age,
+      modifier = modifier.constrainAs(ageItem) {
+        start.linkTo(divider.end)
+        centerVerticallyTo(parent)
+      }
+    )
+  }
+}
+
+@Composable @Preview
+fun ProfileInfoPreview(){
+  ProfileInfo()
 }
 
 @Composable
